ffe6ac5bf4074bc023d54c3c8520342982707d66
alexey12546 <kit.trusov2003@gmail.com>
2023-10-10 10:57:25 +0300

    chistka koda

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <vector>
 #include <iomanip>
 
-
 using namespace std;
+void print(vector<vector<double>> matr);
 
 int main()
 {
@@ -16,15 +16,7 @@ vector<double> str3{1.15,2.70,3.58,37.11};
 vector<vector<double>> matr{str1,str2,str3};
 vector<vector<double>> matr1{str1,str2,str3};
 double max=matr[0][0];
-cout<<setw(6);
-for (size_t i = 0; i < 3;i++)
-{
-    for (size_t j = 0; j < 4; j++)
-    {
-        cout<<matr[i][j]<<setw(6);
-    }
-    cout<<endl;
-}
+print(matr);
 //iter1
 int index=0;
 for (size_t i = 1; i < 3; i++)
@@ -36,44 +28,21 @@ for (size_t i = 1; i < 3; i++)
        }
 }
 swap(matr[0],matr[index]);
-
-//iter2
 double perviy=matr[0][0];
+double elem=matr[1][0];
 for (size_t j = 0; j < 4; j++)
 {
     matr[0][j]=matr[0][j]/perviy;
+    matr[1][j]=matr[0][j]*elem*(-1)+matr[1][j];
 }
-//3 iter
-
-for (size_t i = 1; i < 2; i++)
+elem=matr[2][0];
+for (size_t j = 0; j < 4; j++)
 {
-    double elem=matr[1][0];
-    for (size_t j = 0; j < 4; j++)
-    {
-        matr[i][j]=matr[0][j]*elem*(-1)+matr[i][j];
-    }
+    matr[2][j]=matr[0][j]*elem*(-1)+matr[2][j];
 }
-for (size_t i = 2; i < 3; i++)
-{
-    double elem=matr[2][0];
-    for (size_t j = 0; j < 4; j++)
-    {
-        matr[i][j]=matr[0][j]*elem*(-1)+matr[i][j];
-    }
-}
-
 double max1=matr[1][1];
 cout<<"1 iteraziya"<<endl;
-cout<<setw(10);
-for (size_t i = 0; i < 3;i++)
-{
-    for (size_t j = 0; j < 4; j++)
-    {
-        cout<<matr[i][j]<<setw(10);
-    }
-    cout<<endl;
-}
-
+print(matr);
 //iter 4
 int index1=1;
 for (size_t i = 1; i < 3; i++)
@@ -85,32 +54,15 @@ for (size_t i = 1; i < 3; i++)
        }
 }
 swap(matr[1],matr[index1]);
-
 double vtoroy=matr[1][1];
+double elem1=matr[2][1];
 for (size_t j = 0; j < 4; j++)
 {
     matr[1][j]=matr[1][j]/vtoroy;
+    matr[2][j]=matr[1][j]*elem1*(-1)+matr[2][j];
 }
-
-for (size_t i = 2; i < 3; i++)
-{
-    double elem1=matr[2][1];
-    for (size_t j = 0; j < 4; j++)
-    {
-        matr[i][j]=matr[1][j]*elem1*(-1)+matr[i][j];
-    }
-}
-
 cout<<"2 iteraziya"<<endl;
-cout<<setw(10);
-for (size_t i = 0; i < 3;i++)
-{
-    for (size_t j = 0; j < 4; j++)
-    {
-        cout<<matr[i][j]<<setw(10);
-    }
-    cout<<endl;
-}
+print(matr);
 x3=matr[2][3]/matr[2][2];
 x2=matr[1][3]-x3*matr[1][2];
 x1=matr[0][3]-x2*matr[0][1]-x3*matr[0][2];
@@ -127,9 +79,6 @@ for (size_t i = 1; i < 3; i++)
     }
     
 }
-
-
-
 cout<<"VECTOR NEVYAZKI:"<<endl;
 double vectornev[3]={0,0,0};
 for (size_t i = 0; i < 3; i++)
@@ -147,14 +96,11 @@ for (size_t i = 1; i < 3; i++)
     }
 }
 cout<<norma<<endl;
-
 vector<double> str4{2.75,1.78,1.11,vectornev[0]};
 vector<double> str5{3.28,0.71,1.15,vectornev[1]};
 vector<double> str6{1.15,2.70,3.58,vectornev[2]};
 vector<vector<double>> matr2{str4,str5,str6};
-
 double max2=matr2[0][0];
-
 int index2=0;
 for (size_t i = 1; i < 3; i++)
 {
@@ -165,44 +111,22 @@ for (size_t i = 1; i < 3; i++)
        }
 }
 swap(matr[0],matr[index]);
-
 //iter2
 double perviy1=matr2[0][0];
+elem=matr2[1][0];
 for (size_t j = 0; j < 4; j++)
 {
     matr2[0][j]=matr2[0][j]/perviy1;
+    matr2[1][j]=matr2[0][j]*elem*(-1)+matr2[1][j];
 }
-//3 iter
-
-for (size_t i = 1; i < 2; i++)
+elem=matr2[2][0];
+for (size_t j = 0; j < 4; j++)
 {
-    double elem=matr2[1][0];
-    for (size_t j = 0; j < 4; j++)
-    {
-        matr2[i][j]=matr2[0][j]*elem*(-1)+matr2[i][j];
-    }
+    matr2[2][j]=matr2[0][j]*elem*(-1)+matr2[2][j];
 }
-for (size_t i = 2; i < 3; i++)
-{
-    double elem=matr2[2][0];
-    for (size_t j = 0; j < 4; j++)
-    {
-        matr2[i][j]=matr2[0][j]*elem*(-1)+matr2[i][j];
-    }
-}
-
 double max3=matr2[1][1];
 cout<<"1 iteraziya"<<endl;
-cout<<setw(10);
-for (size_t i = 0; i < 3;i++)
-{
-    for (size_t j = 0; j < 4; j++)
-    {
-        cout<<matr2[i][j]<<setw(10);
-    }
-    cout<<endl;
-}
-
+print(matr2);
 //iter 4
 int index3=1;
 for (size_t i = 1; i < 3; i++)
@@ -214,32 +138,15 @@ for (size_t i = 1; i < 3; i++)
        }
 }
 swap(matr2[1],matr2[index3]);
-
 double vtoroy1=matr2[1][1];
+elem=matr2[2][1];
 for (size_t j = 0; j < 4; j++)
 {
     matr2[1][j]=matr2[1][j]/vtoroy1;
+    matr2[2][j]=matr2[1][j]*elem*(-1)+matr2[2][j];
 }
-
-for (size_t i = 2; i < 3; i++)
-{
-    double elem1=matr2[2][1];
-    for (size_t j = 0; j < 4; j++)
-    {
-        matr2[i][j]=matr2[1][j]*elem1*(-1)+matr2[i][j];
-    }
-}
-
 cout<<"2 iteraziya"<<endl;
-cout<<setw(10);
-for (size_t i = 0; i < 3;i++)
-{
-    for (size_t j = 0; j < 4; j++)
-    {
-        cout<<matr2[i][j]<<setw(10);
-    }
-    cout<<endl;
-}
+print(matr2);
 x3=matr2[2][3]/matr2[2][2];
 x2=matr2[1][3]-x3*matr2[1][2];
 x1=matr2[0][3]-x2*matr2[0][1]-x3*matr2[0][2];
@@ -255,7 +162,26 @@ for (size_t i = 1; i < 3; i++)
     }
 }
 double ocenkOtnPogr=abs(Eabsol-norma)/norma;
-cout<<"Ocenka itnositelnoy pogreshnosti:"<<ocenkOtnPogr<<"%";
-
+cout<<"Ocenka otnositelnoy pogreshnosti:"<<ocenkOtnPogr<<"%";
 return 0;
+}
+
+void print(vector<vector<double>> matr)
+{
+    cout<<setw(14);
+    for (size_t i = 0; i < 3;i++)
+{
+    for (size_t j = 0; j < 4; j++)
+    {
+        if (i<2)
+        {
+            cout<<matr[i][j]<<setw(14);
+        }
+        else if(i==2 && j==3)
+        cout<<matr[i][j];
+        else
+        cout<<matr[i][j]<<setw(14);
+    }
+    cout<<endl;
+}
 }
